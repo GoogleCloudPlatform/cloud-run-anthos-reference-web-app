1ac90746bca8586fe5b0e61901c6b8c7c2c3f833
Shaoxuan Zhang <shaoxuanzhang@google.com>
2020-05-27 19:30:49 -0700

    fix for type completion

--- webui/cypress/support/step_definitions/logins.steps.ts
+++ webui/cypress/support/step_definitions/logins.steps.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/// <reference types="Cypress" />
 
 import { Given } from 'cypress-cucumber-preprocessor/steps';
 
